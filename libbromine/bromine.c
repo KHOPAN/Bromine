0fa66f1e47563818c93222ef5ac375893e69b035
KHOPAN <puthipnakchan@gmail.com>
2024-10-29 17:32:50 +0700

    Rename initialize.c to bromine.c

--- libbromine/bromine.c
+++ libbromine/bromine.c
@@ -1,9 +1,13 @@
 #include "bromine.h"
 
+static LRESULT CALLBACK procedure(_In_ HWND window, _In_ UINT message, _In_ WPARAM wparam, _In_ LPARAM lparam) {
+	return DefWindowProcW(window, message, wparam, lparam);
+}
+
 BROMINEERROR BromineInitialize() {
 	WNDCLASSEXW windowClass = {0};
 	windowClass.cbSize = sizeof(WNDCLASSEXW);
-	windowClass.lpfnWndProc = DefWindowProcW;
+	windowClass.lpfnWndProc = procedure;
 	windowClass.lpszClassName = BROMINE_CLASS;
 	return RegisterClassExW(&windowClass) ? BROMINE_ERROR_SUCCESS : BROMINE_ERROR_WIN32;
 }
