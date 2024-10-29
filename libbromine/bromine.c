b574a99856d9f27f08aea6ac736658661ff918e7
KHOPAN <puthipnakchan@gmail.com>
2024-10-29 19:37:25 +0700

    Add paint handler

--- libbromine/bromine.c
+++ libbromine/bromine.c
@@ -7,6 +7,17 @@ static LRESULT CALLBACK procedure(_In_ HWND window, _In_ UINT message, _In_ WPAR
 		parameter->function(parameter->parameter);
 		return 0;
 	}
+	case WM_PAINT: {
+		PAINTSTRUCT paintStruct;
+		HDC context = BeginPaint(window, &paintStruct);
+		HBRUSH brush = GetStockObject(DC_BRUSH);
+		SetDCBrushColor(context, RGB(0, 255, 0));
+		RECT bounds;
+		GetClientRect(window, &bounds);
+		FillRect(context, &bounds, brush);
+		EndPaint(window, &paintStruct);
+		return 0;
+	}
 	}
 
 	return DefWindowProcW(window, message, wparam, lparam);
